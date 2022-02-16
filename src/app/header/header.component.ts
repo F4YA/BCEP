76653711454957de8e39227f3018669f896f6a4e
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2022-02-16 19:41:34 -0300

    header, home e footer modificados

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public menuIsActived: Boolean | undefined;
+  ngOnInit(): void {
+    this.menuIsActived = false;
+  }
+
+  public showMenu() {
+    this.menuIsActived = !this.menuIsActived
+    console.log(this.menuIsActived)
+  }
 
 }
