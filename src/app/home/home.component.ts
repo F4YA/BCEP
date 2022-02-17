eb87529c258e0e53855c9198c0a0022f4f03eadb
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2022-02-17 20:06:24 -0300

    adicionado validação para o campo de buscar cep

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public IsInvalid: boolean | undefined;
+  public _regex: RegExp = new RegExp('([0-9]{5})-([0-9]{3})', 'g');
+  public cepField: HTMLInputElement | undefined;
 
   ngOnInit(): void {
+    this.IsInvalid = false;
+  }
+
+  public ValidCepField() {
+    this.cepField = document.getElementById('cepField') as HTMLInputElement;
+    this.cepField.value = this.cepField.value.replace(/(\d{5})(\d{3})/, function(regex, a, b) {
+      return a+'-'+b;
+    })
+    if (!this._regex.test(this.cepField.value)) {
+      this.IsInvalid = true;
+      this.cepField.style.borderColor = "#FF8000";
+    } else {
+      this.IsInvalid = false;
+      this.cepField.style.borderColor = "#5C6BC0";
+    }
+  }
+
+  public ShowNotification(argument: boolean) {
+
   }
 
 }
