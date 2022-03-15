b213442e75764f1550224de654cb53391ffc5337
Osvaldo Carvalho <osvaldocarvalho220@gmail.com>
2022-03-15 17:44:48 -0300

    Criando método getCep que recebe um cep e retorna um objeto Cep

--- src/app/cep/cep.service.ts
+++ src/app/cep/cep.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Cep } from "./cep";
 
 @Injectable()
 export class CepService{
-
+  protected urlService: string = "https://brasilapi.com.br/api/cep/v2/";
+  constructor(private httpClient: HttpClient){}
+  private getCep(cep:string): Observable<Cep>{
+    return this.httpClient.get<Cep>(this.urlService + cep);
+  }
 }
